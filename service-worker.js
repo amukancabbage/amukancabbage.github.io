024331b60432ff6640c0fe24776814dc441c4e1f
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2020-12-27 21:27:17 +0800

    menambah dan menghapus cache

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "001";
+var urlsToCache = [
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/contact.html",
+    "/assets/vendor/materialize/css/materialize.min.css",
+    "/assets/vendor/materialize/js/materialize.min.js",
+    "/assets/js/nav.js"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches
+            .match(event.request, { cacheName: CACHE_NAME })
+            .then(function (response) {
+                if (response) {
+                    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                    return response;
+                }
+
+                console.log(
+                    "ServiceWorker: Memuat aset dari server: ",
+                    event.request.url
+                );
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
