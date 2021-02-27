6956805d82c23e95e48e472150d239af3a924f52
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2021-02-27 10:28:34 +0800

    tes pang

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-if (!workbox) {
+if (workbox) {
     workbox.precaching.precacheAndRoute([
-        { url: "/", revision: '2' },
-        { url: "/nav.html", revision: '2' },
-        { url: "/index.html", revision: '2' },
-        { url: "/team.html", revision: '2' },
-        { url: "/pages/home.html", revision: '2' },
-        { url: "/pages/favorite.html", revision: '2' },
-        { url: "/assets/vendor/materialize/css/materialize.min.css", revision: '2' },
-        { url: "/assets/vendor/materialize/js/materialize.min.js", revision: '2' },
-        { url: "/assets/vendor/jakearchibald/js/idb.js", revision: '2' },
-        { url: "/assets/js/api.js", revision: '2' },
-        { url: "/assets/js/auth.js", revision: '2' },
-        { url: "/assets/js/db.js", revision: '2' },
-        { url: "/assets/js/lib.js", revision: '2' },
-        { url: "/assets/js/login.js", revision: '2' },
-        { url: "/assets/js/nav.js", revision: '2' },
-        { url: "/service-worker-register.js", revision: '2' },
-        { url: "/favicon.ico", revision: '2' },
-        { url: "/manifest.json", revision: '2' },
-        { url: "https://fonts.googleapis.com/icon?family=Material+Icons", revision: '2' },
-        { url: "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2", revision: '2' },
-        { url: "https://unpkg.com/snarkdown@1.0.2/dist/snarkdown.umd.js", revision: '2' },
-        { url: "/assets/image/dikbud-192x192.png", revision: '2' },
-        { url: "/assets/image/seriea-icon-400x400.png", revision: '2' },
-        { url: "/assets/image/seriea-icon-512x512.png", revision: '2' }
+        { url: "/", revision: '3' },
+        { url: "/nav.html", revision: '3' },
+        { url: "/index.html", revision: '3' },
+        { url: "/team.html", revision: '3' },
+        { url: "/pages/home.html", revision: '3' },
+        { url: "/pages/favorite.html", revision: '3' },
+        { url: "/assets/vendor/materialize/css/materialize.min.css", revision: '3' },
+        { url: "/assets/vendor/materialize/js/materialize.min.js", revision: '3' },
+        { url: "/assets/vendor/jakearchibald/js/idb.js", revision: '3' },
+        { url: "/assets/js/api.js", revision: '3' },
+        { url: "/assets/js/auth.js", revision: '3' },
+        { url: "/assets/js/db.js", revision: '3' },
+        { url: "/assets/js/lib.js", revision: '3' },
+        { url: "/assets/js/login.js", revision: '3' },
+        { url: "/assets/js/nav.js", revision: '3' },
+        { url: "/service-worker-register.js", revision: '3' },
+        { url: "/favicon.ico", revision: '3' },
+        { url: "/manifest.json", revision: '3' },
+        { url: "https://fonts.googleapis.com/icon?family=Material+Icons", revision: '3' },
+        { url: "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2", revision: '3' },
+        { url: "https://unpkg.com/snarkdown@1.0.2/dist/snarkdown.umd.js", revision: '3' },
+        { url: "/assets/image/dikbud-192x192.png", revision: '3' },
+        { url: "/assets/image/seriea-icon-400x400.png", revision: '3' },
+        { url: "/assets/image/seriea-icon-512x512.png", revision: '3' }
     ], {
         ignoreUrlParametersMatching: [/.*/]
     });
