936a0e800a60f573f179a1128e8fdb074ae53b25
mirzayogy <mirza.yogy@gmail.com>
2022-08-24 14:33:00 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-const version = '8';
+const version = '9';
 
 if (workbox) {
     workbox.precaching.precacheAndRoute([
